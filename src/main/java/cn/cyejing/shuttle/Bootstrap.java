397ab649cc503d5327af8c702d50a2662f29e2cd
Chen.YeJing <jing23501@qq.com>
2020-06-22 17:32:12 +0800

    update 0.2.0-SNAPSHOT

---
+++ src/main/java/cn/cyejing/shuttle/Bootstrap.java
@@ -0,0 +1,18 @@
+/*
+ * frxs Inc.  湖南兴盛优选电子商务有限公司.
+ * Copyright (c) 2017-2019. All Rights Reserved.
+ */
+package cn.cyejing.shuttle;
+
+/**
+ * <B>主类名称：</B><BR>
+ * <B>概要说明：</B><BR>
+ *
+ * @author Born
+ * @since 2020年06月22日 5:31 下午
+ */
+public class Bootstrap {
+    public static void main(String[] args) {
+
+    }
+}
